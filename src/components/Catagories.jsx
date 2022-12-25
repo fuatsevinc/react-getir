94a868190c7bacc71bfcfb54ce8a9415d7c26eaf
fuatsevinc <fuatsevinc@icloud.com>
2022-12-25 22:51:00 +0100

    update

--- src/components/Catagories.jsx
+++ src/components/Catagories.jsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
-import categoryData from 'api/categories.json';
-import Category from "./ui/Category";
+// import { useEffect, useState } from "react";
+// import Catagories from 'api/categories.json';
+// import Category from "./ui/Category";
 
-export default function Catagories() {
+// export default function Catagories() {
      
-    const [ categories, setCategories] = useState([]);
+//     const [ categories, setCategories] = useState([]);
     
-    useEffect (() => {
-      setCategories(categoryData);
-    }, [])
+//     useEffect (() => {
+//       setCategories(categories);
+//     }, [])
 
-    return (
-      <div className="bg-white py-4">
-        <div className="container mx-auto">
-          <h3 className="text-sm font-semibold mb-3"> Kategoriler</h3>
-          <div className="grid grid-cols-10">
-            {categories && categories.map((category, index) => <Category key={index} category={category} />)}
-          </div>
-        </div>
+//     return (
+//       <div className="bg-white py-4">
+//         <div className="container mx-auto">
+//           <h3 className="text-sm font-semibold mb-3"> Kategoriler</h3>
+//           <div className="grid grid-cols-10">
+//             {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+//           </div>
+//         </div>
        
-      </div>
-    )
-  }
+//       </div>
+//     )
+//   }
--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,29 +1,7 @@
-import { useEffect, useState } from "react";
-import categoryData from 'api/categories.json';
-import Category from "./ui/Category";
-
-export default function Favorites() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 300,
-    arrows: false,
-    slidesToShow: 2,
-    slidesToScroll: 2
-  };
-
-return (
-    <div className="relative before:bg-gradient-to-r before:bg-primary-brand-color before:to-transparent before:absolute before:insert-0 before:w-full before:h-full before:z-1 ">
-      <Slider {...settings }>
-      
-        <div>
-          <img className="w-full h-[500px] object-cover" src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-4.jpg"  />
-        </div>
-
-        <div>
-        <img className="w-full h-[500px] object-cover" src="https://cdn.getir.com/getirweb-images/common/hero-posters/getir-mainpage-1.jpg"  />
-      </div>
-      </Slider>
+export default function Favorites () {
+  return (
+    <div>
+      Kartlar
     </div>
   )
 }
