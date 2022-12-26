831529d110f80e438e64d9a7cc6b165a00c3d87e
fuatsevinc <fuatsevinc@icloud.com>
2022-12-26 22:55:55 +0100

    update

--- src/components/Catagories.jsx
+++ src/components/Catagories.jsx
@@ -1,24 +1,40 @@
-// import { useEffect, useState } from "react";
-// import Catagories from 'api/categories.json';
-// import Category from "./ui/Category";
+export default function Categories () {
+    return (
+      <div>
+        Kategoriler
+      </div>
+    )
+  }
 
-// export default function Catagories() {
+
+
+
+
+
+
+
+
+//  import { useEffect, useState } from "react";
+//  import Catagories from 'api/categories.json';
+//  import Category from "./ui/Category";
+
+//  export default function Catagories() {
      
-//     const [ categories, setCategories] = useState([]);
+//      const [ categories, setCategories] = useState([]);
     
-//     useEffect (() => {
-//       setCategories(categories);
-//     }, [])
+//      useEffect (() => {
+//        setCategories(categories);
+//      }, [])
 
-//     return (
-//       <div className="bg-white py-4">
-//         <div className="container mx-auto">
-//           <h3 className="text-sm font-semibold mb-3"> Kategoriler</h3>
-//           <div className="grid grid-cols-10">
-//             {categories && categories.map((category, index) => <Category key={index} category={category} />)}
-//           </div>
-//         </div>
+//      return (
+//        <div className="bg-white py-4">
+//          <div className="container mx-auto">
+//            <h3 className="text-sm font-semibold mb-3"> Kategoriler</h3>
+//            <div className="grid grid-cols-10">
+//              {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+//            </div>
+//          </div>
        
-//       </div>
-//     )
-//   }
+//        </div>
+//      )
+//    }
